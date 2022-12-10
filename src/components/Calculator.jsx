739261b1dc500980685b20e8ea27b4d194fe4b5e
Sayem Zaman <fzsayemofficial@gmail.com>
2022-12-11 00:19:52 +0600

    first time push

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import { Button, Container, Current, Previous, Screen } from "../styles/Main";
+
+const Calculator = () => {
+  const [current, SetCurrent] = useState("");
+  const [previous, SetPrevious] = useState("");
+  const [operationValue, SetOperationValue] = useState("");
+
+  const numberHandler = (e) => {
+    const value = e.target.innerText;
+    if (value === "." && current.includes(".")) return;
+    SetCurrent(current + value);
+  };
+
+  const operationHandler = (e) => {
+    const value = e.target.innerText;
+    let oparateValue = compute();
+
+    if (!current) return;
+
+    if (previous) {
+      SetPrevious(oparateValue);
+    } else {
+      SetPrevious(current);
+    }
+    SetOperationValue(value);
+
+    SetCurrent("");
+  };
+
+  const equalHandler = () => {
+    let value = compute();
+
+    if (value === undefined || value == null) return;
+
+    SetCurrent(value);
+    SetPrevious("");
+    SetOperationValue("");
+  };
+
+  const cleaerHandler = () => {
+    SetCurrent("");
+    SetPrevious("");
+    SetOperationValue("");
+  };
+
+  const deleteHandler = () => {
+    SetCurrent(String(current).slice(0, -1));
+  };
+
+  const compute = () => {
+    let result;
+    let previousNumber = parseFloat(previous);
+    let currentNumber = parseFloat(current);
+    if (isNaN(previousNumber) || isNaN(currentNumber)) return;
+
+    switch (operationValue) {
+      case "+":
+        result = previousNumber + currentNumber;
+        break;
+      case "×":
+        result = previousNumber * currentNumber;
+        break;
+      case "-":
+        result = previousNumber - currentNumber;
+        break;
+      case "÷":
+        result = previousNumber / currentNumber;
+        break;
+      default:
+        return;
+    }
+    return result;
+  };
+  return (
+    <>
+      <Container>
+        <Screen>
+          <Current carr={current}>{current}</Current>
+          <Previous>
+            {previous}
+            <span > {operationValue} </span>
+            {previous ? current : ""}
+          </Previous>
+        </Screen>
+        <Button spanTwo={2} operation onClick={cleaerHandler}>
+          AC
+        </Button>
+        <Button operation onClick={deleteHandler}>
+          C
+        </Button>
+        <Button operation onClick={operationHandler}>
+          ÷
+        </Button>
+        <Button onClick={numberHandler}>7</Button>
+        <Button onClick={numberHandler}>8</Button>
+        <Button onClick={numberHandler}>9</Button>
+        <Button operation onClick={operationHandler}>
+          ×
+        </Button>
+        <Button onClick={numberHandler}>4</Button>
+        <Button onClick={numberHandler}>5</Button>
+        <Button onClick={numberHandler}>6</Button>
+        <Button operation onClick={operationHandler}>
+          -
+        </Button>
+        <Button onClick={numberHandler}>1</Button>
+        <Button onClick={numberHandler}>2</Button>
+        <Button onClick={numberHandler}>3</Button>
+        <Button operation spanTwoRow={2} onClick={operationHandler}>
+          +
+        </Button>
+        <Button onClick={numberHandler}>.</Button>
+        <Button onClick={numberHandler}>0</Button>
+        <Button operation onClick={equalHandler}>
+          =
+        </Button>
+      </Container>
+    </>
+  );
+};
+
+export default Calculator;
---
+++ src/styles/Main.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 4.5rem);
+  grid-template-rows: minmax(150px, auto) repeat(5, 4.5rem);
+  grid-gap: 1rem;
+  padding: 30px 25px;
+  text-align: center;
+  justify-content: center;
+  align-content: center;
+  margin-top: 100px;
+
+  background: #1a1b28;
+`;
+
+export const Screen = styled.div`
+  grid-column: 1 / -1;
+  font-family: 'Berkshire Swash', cursive;
+  color: white;
+  padding: 20px 0px 20px 20px;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  text-align: right;
+  word-wrap: break-word;
+  word-break: break-all;
+`;
+
+export const Current = styled.div`
+  font-size: 2.5rem;
+  font-weight: bold;
+  padding: 20px 0;
+  ${({ carr }) =>
+    carr && "position: relative; border-bottom: 1px solid #52cadc28;"}
+
+  &::before {
+    position: absolute;
+    content: "=";
+    right: 100%;
+    font-size: 2.5rem;
+    color: #52c9dc;
+  }
+`;
+
+export const Previous = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding-top: 10px;
+
+  span {
+    color: #52c9dc;
+  }
+`;
+
+export const Button = styled.button`
+  cursor: pointer;
+  font-family: 'Berkshire Swash', cursive;
+  border-radius: 6px;
+  font-size: 1.8rem;
+  color: white;
+  border: none;
+  background-color: #1e2435;
+  &:hover {
+    background-color: #242730;
+  }
+
+  ${({ spanTwo }) =>
+    spanTwo &&
+    `grid-column:span ${spanTwo};
+  `}
+
+  ${({ spanTwoRow }) =>
+    spanTwoRow &&
+    `grid-row:span ${spanTwoRow};
+  `}
+    ${({ operation }) =>
+    operation &&
+    `background-color: #52C9DC ;
+  };
+  `}
+`;
